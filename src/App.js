9118e603a8ec524a14a460c78c310877acef7850
SMIT\smit <smitkakadiya2003@gmail.com>
2023-07-24 15:29:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Data from './Components/Data';
+import Props from './Components/Props';
+
+function App() {
+
+  return (
+    <>
+      <Props record={Data}/>
+    </>
+  );
+}
+
+export default App;
