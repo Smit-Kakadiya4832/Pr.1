2edd6dab4c3bc6b70b27efd2403eb430028e7a15
SMIT\smit <smitkakadiya2003@gmail.com>
2023-08-01 15:18:29 +0530

    PR-2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { useState } from 'react';
 import './App.css';
-import Data from './Components/Data';
-import Props from './Components/Props';
+import { useEffect } from 'react';
 
 function App() {
 
+    const [no , setNo] = useState (0)
+    const [result , setResult] = useState (0)
+
+    const btnPlus = () => {
+      setNo( no + 1 )
+    }
+    useEffect (() => {
+      console.log(no);
+    },[no])
   return (
     <>
-      <Props record={Data}/>
+    <h1>No :- {no}</h1>
+    <button onClick={()=> btnPlus()}>Click</button>
     </>
   );
 }
